8db04e42df5a15a0c0ca9b9b26ffa79038d64f28
OLAEMJO8 <olaecheamariajose@gmail.com>
2024-03-12 15:59:11 -0300

    feat. creacion de ruta y controlador para obtener un libro por ID

--- server/controllers/books.controller.js
+++ server/controllers/books.controller.js
@@ -9,3 +9,17 @@ export const getBooks = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Controlador para obtener un libro por su ID
+export const getBook = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await Book.findById(id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res.status(200).json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/routes/books.routes.js
+++ server/routes/books.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 
-import { getBooks} from '../controllers/books.controller.js';
+import { getBooks, getBook} from '../controllers/books.controller.js';
 
 
 const router = Router();
 
 // GET all Books
 router.get("/", getBooks);
+
+// GET one Book
+router.get("/:id", getBook);
+
 
 export default router;
