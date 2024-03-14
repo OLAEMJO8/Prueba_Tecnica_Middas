2b55f7531e6ec9534d8fab228855ee3c5862353c
OLAEMJO8 <olaecheamariajose@gmail.com>
2024-03-14 13:47:36 -0300

    edit. add. nuevas rutas agregar, editar, favorito

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import Form from "./components/Form.jsx";
 import { Navbar } from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
-
+import FavoriteBooks from "./pages/FavortitesBooks.jsx";
 
 
 
@@ -15,6 +15,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/add" element={<Form />} />
+          <Route path="/edit/:id" element={<Form />} />
+          <Route path="/fav" element={<FavoriteBooks />} />
         </Routes>
       </BrowserRouter>
     </div>
